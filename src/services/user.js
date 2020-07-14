c5a9b3a9ac1a78d1f623eac69de525434a1b2421
Emil Mordarski <mordarskiemil@gmail.com>
2020-07-14 20:21:15 +0200

    bug fixed

--- src/services/user.js
+++ src/services/user.js
@@ -2,7 +2,7 @@ const User = require('../models/User')
 
 module.exports = {
     getUser: async (id) => {
-        const user = User.findById({ id })
+        const user = User.findById(id)
         return user
     },
     createUser: async (name) => {
