99981d75de6b1a781f27f69de0c4abd934d0a5c2
Emil Mordarski <mordarskiemil@gmail.com>
2020-07-14 20:15:17 +0200

    added more test and fixes

--- test/services/user.test.js
+++ test/services/user.test.js
@@ -1,10 +1,22 @@
 const userService = require('../../src/services/user')
 const User = require('../../src/models/User')
-const { connectDb } = require('../helpers')
+const { connectDb, closeDb } = require('../helpers')
 
 describe('userService', () => {
     beforeAll(async () => {
         await connectDb()
+    })
+
+    afterAll(async () => {
+        await closeDb()
+    })
+
+    test('should get user', async () => {
+        const user = new User({ name: 'wladyslaw' })
+        await user.save()
+        const obtainedUser = await userService.getUser(user._id)
+        await User.findOneAndDelete({ name: 'wladyslaw' })
+        expect(obtainedUser).toHaveProperty('name', 'wladyslaw')
     })
 
     test('should create user ', async () => {
@@ -13,6 +25,15 @@ describe('userService', () => {
         await User.findOneAndDelete({ name: foundUser.name })
         expect(foundUser).toHaveProperty('name', 'marian')
     })
+
+    test('should delete user', async () => {
+        const user = new User({ name: 'adrian' })
+        await user.save()
+        await userService.deleteUser(user._id)
+        const foundDeletedUser = await User.findById(user._id)
+        expect(foundDeletedUser).toBeNull()
+    })
+
 });
 
 
