3df4b605ca3113fde6c4ce342224c908987e6a24
Emil Mordarski <mordarskiemil@gmail.com>
2020-07-14 20:13:14 +0200

    refactoring

--- test/seed.js
+++ test/seed.js
@@ -1,24 +1,11 @@
-const { connectDb } = require('./helpers')
 const userFactory = require('./factories/user')
 const chatRoomFactory = require('./factories/chatRoom')
 const messageFactory = require('./factories/message')
 
-async function main() {
-    await connectDb()
-    await seedDatabase()
-}
-
-const seedDatabase = async () => {
+module.exports = async () => {
     const users = new Array(10).fill(null).map(() => userFactory.createUser())
     const chatRooms = new Array(10).fill(null).map(() => chatRoomFactory.createChatRoom(users))
     const messages = new Array(20).fill(null).map(() => messageFactory.createMessage(users, chatRooms))
-
-    console.log(`chatRooms`)
-    chatRooms.forEach(r => console.log(`room.name = ${r.name}`))
-    console.log(`users`)
-    users.forEach(r => console.log(`user.name = ${r.name}`))
-    console.log(`Messages`)
-    messages.forEach(r => console.log(`message.content = ${r.content}`))
 
     try {
         await Promise.all(
@@ -32,5 +19,3 @@ const seedDatabase = async () => {
         console.log(`Saving data error: ${err}`)
     }
 }
-
-module.exports = { main, seedDatabase }
