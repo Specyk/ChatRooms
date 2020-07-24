06d6226274f12848622f064fd1af406cd312668a
Emil Mordarski <mordarskiemil@gmail.com>
2020-07-24 22:30:59 +0200

    added reference to redux store

--- src/client/src/components/Header/Header.jsx
+++ src/client/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import RoomsCount from './RoomsCount'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
-    const [roomsCount, setRoomsCount] = useState(14)
+    const roomsCount = useSelector(state => state.rooms.length)
+
     return (
         <div className="navbar navbar-light sticky-top bg-light flex-md-nowrap p-0">
             <h1><Link to="/">ChatRooms</Link></h1>
