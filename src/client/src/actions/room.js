f34da21fcdea1c556c18431dcdc97c873c9996da
Emil Mordarski <mordarskiemil@gmail.com>
2020-07-26 21:09:41 +0200

    added selectRoom api request

--- src/client/src/actions/room.js
+++ src/client/src/actions/room.js
@@ -1,4 +1,10 @@
-export const selectRoom = (payload) => ({
-    type: "SELECT_ROOM",
-    payload
-})
+import chatRoomsApi from '../api/chatRooms'
+
+export const selectRoom = (chatId) => async (dispatch, getState) => {
+    const chatRoom = await chatRoomsApi.get(`/chatRooms/${chatId}`)
+    console.log(`!@# room: ${chatRoom.data}`)
+    return dispatch({
+        type: "SELECT_ROOM",
+        payload: chatRoom.data
+    })
+}
