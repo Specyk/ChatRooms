bb1f1cd8ea2d5e8db0b93dc10002bea6955cc6bd
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-02 20:59:47 +0200

    added middleware for redux dev tools

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from './reducers'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+    reducers,
+    composeEnhancers(
+        applyMiddleware(thunk),
+        applyMiddleware())
+)
+
 ReactDOM.render((
-    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+    <Provider store={store}>
         <App />
     </Provider>
 ), document.getElementById('root'))
