17837ec8799f59bd05ade180214b778eda44b511
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-02 21:11:48 +0200

    bug fixed

--- src/client/src/components/Chat/SendMessageForm.jsx
+++ src/client/src/components/Chat/SendMessageForm.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { sendMessage } from '../../actions/messages.js'
 
-export default function SendMessageForm({ submitHandler }) {
+export default function SendMessageForm({ onSendMessage }) {
     const [message, setMessage] = useState('')
-    const dispatch = useDispatch()
-    const onFormSubmit = (message) => {
-        dispatch(sendMessage(message))
-    }
+
     return (
-        <form onSubmit={onFormSubmit(message)}>
-            <div class="form-group">
-                <input type="text" onChange={e => setMessage(e.target.value)} class="form-control" placeholder="Your message..." />
+        <form onSubmit={() => onSendMessage(message)}>
+            <div className="form-group">
+                <input type="text" onChange={e => setMessage(e.target.value)} className="form-control" placeholder="Your message..." />
                 <input type="submit" value="Send" />
             </div>
         </form>
