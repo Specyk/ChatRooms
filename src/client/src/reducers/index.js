112bb5b61cde6bf6f561408f243e057f6c5c64de
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-02 21:07:35 +0200

    updates in reducers

--- src/client/src/reducers/index.js
+++ src/client/src/reducers/index.js
@@ -1,17 +1,13 @@
 import { combineReducers } from 'redux'
 
-import messagesReducer from './messages'
 import roomsReducer from './rooms'
 import selectedRoomReducer from './selectedRoom'
 import userReducer from './user'
-import membersReducer from './members'
 
 const reducers = {
     rooms: roomsReducer,
     user: userReducer,
     selectedRoom: selectedRoomReducer,
-    members: membersReducer,
-    messages: messagesReducer
 }
 
 export default combineReducers(reducers)
