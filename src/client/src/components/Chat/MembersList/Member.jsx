ae8d20a7508c21896cd1f8b1fc4e5bf0d48f4b2c
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-02 21:12:59 +0200

    cosmetic changes

--- src/client/src/components/Chat/MembersList/Member.jsx
+++ src/client/src/components/Chat/MembersList/Member.jsx
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react'
 export default function Member({ username }) {
     return (
         <Fragment>
-            <img class="mr-3" src="..." alt={username} />
-            <div class="media-body">
-                <h5 class="mt-0 mb-1">{username}</h5>
+            <img className="mr-3" src="..." alt={username} />
+            <div className="media-body">
+                <h5 className="mt-0 mb-1">{username}</h5>
             </div>
         </Fragment>
     )
--- src/client/src/components/Chat/MessagesList/Message.jsx
+++ src/client/src/components/Chat/MessagesList/Message.jsx
@@ -3,9 +3,9 @@ import React, { Fragment } from 'react'
 export default function Message({ author, date, children }) {
     return (
         <Fragment>
-            <img class="mr-3" src="..." alt={author} />
-            <div class="media-body">
-                <h5 class="mt-0 mb-1">{author}</h5>
+            <img className="mr-3" src="..." alt={author} />
+            <div className="media-body">
+                <h5 className="mt-0 mb-1">{author}</h5>
                 <span>{children}</span>
             </div>
         </Fragment>
