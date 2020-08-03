6c1a347eb0bc0fd59d0ab9e1456d01f2f7721d01
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-03 17:17:30 +0200

    minor refactor

--- src/client/src/actions/roomsActions.js
+++ src/client/src/actions/roomsActions.js
@@ -1,17 +1,28 @@
 import chatRoomsApi from '../api/chatRooms'
 
-export const loadRooms = (rooms) => async (dispatch, getState) => {
+export const REMOVE_ROOM = "REMOVE_ROOM"
+export const FETCH_ROOMS = "FETCH_ROOMS"
+export const CREATE_ROOM = "CREATE_ROOM"
+export const CLICK_CREATE_ROOM = "CLICK_CREATE_ROOM"
+
+export const fetchRooms = (rooms) => async (dispatch, getState) => {
     const response = await chatRoomsApi.get('/chatRooms')
     dispatch({
-        type: "LOAD_ROOMS",
+        type: FETCH_ROOMS,
         payload: response.data
     })
 }
 
-export const addRoom = (room) => async (dispatch) => {
+export const clickCreateRoom = () => {
+    return {
+        type: CLICK_CREATE_ROOM
+    }
+}
+
+export const createRoom = (room) => async (dispatch) => {
     const response = await chatRoomsApi.post('/chatRooms', room)
     dispatch({
-        type: "ADD_ROOM",
+        type: CREATE_ROOM,
         payload: response
     })
 }
@@ -19,7 +30,7 @@ export const addRoom = (room) => async (dispatch) => {
 export const removeRoom = (room) => async (dispatch) => {
     const response = await chatRoomsApi.delete(`/chatRooms/${room._id}`, room)
     return {
-        type: "REMOVE_ROOM",
+        type: REMOVE_ROOM,
         payload: response
     }
 }
--- src/client/src/containers/Header/HeaderContainer.jsx
+++ src/client/src/containers/Header/HeaderContainer.jsx
@@ -3,7 +3,7 @@ import Header from '../../components/Header/Header'
 import { useSelector } from 'react-redux'
 
 export default function HeaderContainer() {
-    const roomsCount = useSelector(state => state.rooms.length)
+    const roomsCount = useSelector(state => state.roomsList.rooms.length)
 
     return (
         <Header roomsCount={roomsCount} />
