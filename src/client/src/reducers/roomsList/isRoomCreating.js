a5812d9af8fa9ae47ad7155c61aaa2b16bbfc627
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-03 17:15:59 +0200

    reducer for createRoomEntry component switch

---
+++ src/client/src/reducers/roomsList/isRoomCreating.js
@@ -0,0 +1,10 @@
+import { CLICK_CREATE_ROOM } from '../../actions/roomsActions'
+
+export default (state = false, { type, payload }) => {
+    switch (type) {
+        case CLICK_CREATE_ROOM:
+            return !state
+        default:
+            return state
+    }
+}
