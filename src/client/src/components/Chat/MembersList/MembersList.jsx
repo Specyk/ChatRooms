9658ece7adaf1a6f14321b51d2fe05960d1aa504
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-04 15:05:09 +0200

    added check if members exist

--- src/client/src/components/Chat/MembersList/MembersList.jsx
+++ src/client/src/components/Chat/MembersList/MembersList.jsx
@@ -5,9 +5,17 @@ export default function MembersList({ membersArr }) {
     const createMember = ({ name }) => (
         <Member username={name} />
     )
+
+    const renderContent = () => {
+        return membersArr.length > 0 ?
+            membersArr.map((m, key) => <li key={key} className="media">{createMember(m)}</li>) :
+            <p>No members</p>
+    }
+
     return (
         <ul className="list-unstyled">
-            {membersArr.map((m, key) => <li key={key} className="media">{createMember(m)}</li>)}
+            {renderContent()}
         </ul>
+
     )
 }
