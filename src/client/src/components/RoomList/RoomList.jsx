ff6fa456272811dac7395035fd4ee3380bac087e
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-13 13:35:40 +0200

    addBtn updated

--- src/client/src/components/RoomList/RoomList.jsx
+++ src/client/src/components/RoomList/RoomList.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import RoomEntries from './RoomEntries'
 import './RoomList.less'
+
+const createAddBtn = (isActive, onClickHandler) => {
+    const className = `addBtn${isActive ? ' active' : ''}`
+    return <button onClick={() => onClickHandler()} className={className}>+</button>
+}
 
 export default function RoomList({ rooms, isAdding, onAddIconClick }) {
     return (
         <div className="RoomList">
             <header>
                 <h3>Room list</h3>
-                <span onClick={() => onAddIconClick()} className="ml-auto mr-4">+</span>
+                {createAddBtn(isAdding, onAddIconClick)}
             </header>
             <main>
                 <RoomEntries isAdding={isAdding} roomsArr={rooms} />
