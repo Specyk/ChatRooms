73a6fa2685e3ac39602e5c7092582ca991340a48
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-13 12:23:30 +0200

    added redux-form reducer

--- src/client/src/reducers/index.js
+++ src/client/src/reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux'
 import roomsListReducer from './roomsList'
 import selectedRoomReducer from './selectedRoom'
 import userReducer from './user'
+import { reducer as reduxReducer } from 'redux-form'
 
 const reducers = {
     roomsList: roomsListReducer,
     user: userReducer,
     selectedRoom: selectedRoomReducer,
+    form: reduxReducer
 }
 
 export default combineReducers(reducers)
