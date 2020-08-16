e8b9f8efe52d8f466c08718c44e705c80523e3ae
Emil Mordarski <mordarskiemil@gmail.com>
2020-08-16 21:32:10 +0200

    added selectedRoom highlight on select

--- src/client/src/containers/RoomList/RoomEntryContainer.jsx
+++ src/client/src/containers/RoomList/RoomEntryContainer.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import RoomEntry from 'components/RoomList/RoomEntry'
 import { useSelector } from 'react-redux'
 
-export default function RoomEntryContainer({ id, name, lastActivityTime }) {
+export default function RoomEntryContainer({ id: thisRoomEntryId, name, lastActivityTime }) {
     const state = useSelector(state => state)
-    const { user, roomsList } = state
-    const members = roomsList.rooms.find(r => r._id === id)
+    const {
+        user,
+        roomsList,
+        selectedRoom: {
+            id: selectedRoomId
+        }
+    } = state
+    const members = roomsList.rooms.find(r => r._id === thisRoomEntryId)
     return (
-        <RoomEntry user={user} id={id} name={name} members={members} lastActivityTime={lastActivityTime} />
+        <RoomEntry user={user} id={thisRoomEntryId} name={name} members={members} lastActivityTime={lastActivityTime} selectedRoomId={selectedRoomId} />
     )
 }
